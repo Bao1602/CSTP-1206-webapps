d049cc683d56ab49909b0fafc9a0a46d41711d13
Bao <000477980@student.vcc.ca>
2024-02-08 23:18:00 -0800

    Get Details function added

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const API_URL = "https://reqres.in/api/users";
 
 let userInfoData = [];
 const userContainer = document.getElementById("user-container");
+const userClickedInfo = document.getElementById("user-clicked-info");
+
 
 async function getUserInfo() {
   // JUST TO UNDERSTAND HOW IT WORKS WITH THEN CATCH BLOCK
@@ -16,7 +18,7 @@ async function getUserInfo() {
     const data = await fetch(API_URL);
     const dataInJson = await data.json();
     userInfoData = dataInJson.data;
-    generateAllCards(userInfoData)
+    generateAllCards(userInfoData);
   } catch (error) {
     console.log("There was an error", error);
     userInfoData = [];
@@ -29,9 +31,9 @@ function createCardUI(user) {
   <img src=${user.avatar} class="card-img-top" alt="...">
   <div class="card-body">
     <h1>${user.first_name} ${user.last_name}</h1>
-    <p class="card-text">${user.email}</p>
+    <p class="card-text">${user.email} ${user.id}</p>
   </div>
-  <button class="btn btn-primary">Get Details</button>
+  <button class="btn-primary" onclick="getUserDetails(${user.id})">Get Details</button> 
 </div>
     `;
 
@@ -44,4 +46,27 @@ function generateAllCards(userData = []) {
     }
 }
 
+function getUserDetails(userId) {
+  const user = userInfoData.find((user) => user.id === userId);
+  userClickedInfo.innerHTML = `
+  <div class="card m-4" style="width: 20rem;">
+  <img src=${user.avatar} class="card-img-top" alt="...">
+  <div class="card-body">
+    <h1 class= "p-3 mb-2 bg-warning text-dark">${user.first_name} ${user.last_name}</h1>
+    <p class="p-3 mb-2 bg-danger text-white">${user.email}</p>
+  </div>`;
+
+  
+}
+
 getUserInfo();
+
+
+//every button have an id numbered
+// when click find the id number then get the data  
+// after that add the data into the container
+
+
+
+
+
